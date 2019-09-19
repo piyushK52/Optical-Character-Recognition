ef31d1d9db7aca5c91ddcd3b3626fe5bd4c9e262
piyush_k <piyush.kumar@amplussolar.com>
2019-09-19 09:56:31 +0530

    view made for uploading file

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('hey');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const multer = require('multer');
+const {TesseractWorker} = require('tesseract.js');
+const worker = new TesseractWorker();
+
+const storage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, "./uploads")
+    },
+    filename: (req, res, cb) => {
+        cb(null, req.file)
+    }
+});
+
+const upload = multer({storage: storage}).single('avatar');
+app.set('view engine','ejs');
+
+// routes
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.post('/upload',(req,res)=>{
+    upload
+});
+
+// start up our server
+const PORT = 500;
+app.listen(PORT, ()=>console.log(`hey I'm running on port ${PORT}`));
